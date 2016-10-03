7a774c5ba57fd04a4331d2cb13802078b86e1c47
Daniel <daniel.kuroski@gmail.com>
2016-10-02 23:08:27 -0300

    18 - Persisting our State with Firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDk4FcqnzA6Fa5OFJdH8Zd9G29MAo8IqbY",
+  authDomain: "kuroski-catch-a-day.firebaseapp.com",
+  databaseURL: "https://kuroski-catch-a-day.firebaseio.com"
+});
+
+export default base;
